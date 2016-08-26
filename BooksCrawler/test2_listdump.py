c60f90ffb32aac472ccbc2e791de55a07fa320d4
mmmaaaxxx77 <mmmaaaxxx77@gmail.com>
2016-08-26 17:58:03 +0800

    add new book list url dump

---
+++ BooksCrawler/test2_listdump.py
@@ -0,0 +1,26 @@
+import time
+from bookthread.bookTask import BookTask
+from crawler import listCrawler
+import urlList
+from bs4 import BeautifulSoup
+import requests
+
+f = file("/Users/johnnytsai/books/list.txt", "w")
+
+for url in urlList.booklist:
+    html = requests.get(url, timeout=(10.0, 10.0)).text
+    soup = BeautifulSoup(html, "html.parser")
+    pageSize = listCrawler.getPageSize(soup)
+    for index in range(pageSize):
+        page = index+1
+        html2 = requests.get(url + str(page), timeout=(10.0, 10.0)).text
+        soup2 = BeautifulSoup(html2, "html.parser")
+        for bookurl in listCrawler.getBookList(soup2):
+            print("get and put url: " + bookurl)
+            total_url_str = bookurl + "\n"
+            f.write(total_url_str)
+        time.sleep(1)
+        print("get page: " + url + str(page))
+    time.sleep(1)
+
+f.close()
